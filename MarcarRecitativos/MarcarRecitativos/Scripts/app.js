142604337814d06af286e1f85458bd969b927a50
Heber Teixeira <heber_navegante@live.com>
2016-12-28 15:27:14 -0200

    First commit

---
+++ MarcarRecitativos/MarcarRecitativos/Scripts/app.js
@@ -0,0 +1,36 @@
+﻿'use strict'
+
+var baseUrl = window.location.protocol + '/' + window.location.hostname;
+
+angular.module("ccbApp", ['ngRoute'])
+
+.constant('_ccb', {
+  'viewPath': '/content/angular/recitativos'
+})
+
+.config([
+  '$routeProvider', '$locationProvider', '_ccb',
+  function ($routeProvider, $locationProvider, viewPath) {
+    viewPath = viewPath.viewPath;
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when('/', {
+        templateUrl: viewPath + '/main.html',
+        controller: 'DefaultCtrl',
+        controllerAs: 'vm'
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
+  }
+])
+
+.controller('DefaultCtrl', [
+  function () {
+    var vm = this;
+    vm.today = new Date();
+    
+  }
+])
