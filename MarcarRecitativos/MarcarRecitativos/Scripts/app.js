87e19cb0b113593c9a86a165899179fc9daa65d7
Heber Teixeira <heber_navegante@live.com>
2016-12-28 22:41:42 -0200

    Criação do layout

--- MarcarRecitativos/MarcarRecitativos/Scripts/app.js
+++ MarcarRecitativos/MarcarRecitativos/Scripts/app.js
@@ -2,22 +2,23 @@
 
 var baseUrl = window.location.protocol + '/' + window.location.hostname;
 
-angular.module("ccbApp", ['ngRoute'])
+angular.module("ccbApp", [
+  'ngRoute',  
+])
 
-.constant('_ccb', {
-  'viewPath': '/content/angular/recitativos'
+.constant('_paths', {
+  'views': '/content/angular/recitativos',
+  'directives': '/content/angular/recitativos/directives'
 })
 
 .config([
-  '$routeProvider', '$locationProvider', '_ccb',
-  function ($routeProvider, $locationProvider, viewPath) {
-    viewPath = viewPath.viewPath;
-
+  '$routeProvider', '$locationProvider', '_paths',
+  function ($routeProvider, $locationProvider, path) {
     $locationProvider.html5Mode(true);
 
     $routeProvider
       .when('/', {
-        templateUrl: viewPath + '/main.html',
+        templateUrl: path.views + '/main.html',
         controller: 'DefaultCtrl',
         controllerAs: 'vm'
       })
@@ -27,10 +28,63 @@ angular.module("ccbApp", ['ngRoute'])
   }
 ])
 
+.directive('contagemMeninas', [
+  '_paths',
+  function(path) {
+    return {
+      restrict: 'E',
+      templateUrl: path.directives + '/meninas.html'
+    }
+  }
+])
+
+.directive('contagemMeninos', [
+  '_paths',
+  function(path) {
+    return {
+      restrict: 'E',
+      templateUrl: path.directives + '/meninos.html'
+    }
+  }
+])
+
 .controller('DefaultCtrl', [
   function () {
     var vm = this;
     vm.today = new Date();
     
+    vm.mocidade = [
+      { gender: 'menina', total: 0 },      
+      { gender: 'moca', total: 0 },
+      { gender: 'menino', total: 0 },
+      { gender: 'moco', total: 0 },
+    ];
+  
+    vm.addFemaleContinuation = function() {
+      vm.mocidade.push({ gender: 'menina', total: 0 });
+    }
+
+    vm.addMaleContinuation = function() {
+      vm.mocidade.push({ gender: 'menino', total: 0 });
+    }
+
+    vm.girlAndYoungWomanFilter = function(item) {
+      return item.gender === 'menina' || item.gender === 'moca';
+    }
+
+    vm.boyAndYoungManFilter = function(item) {
+      return item.gender === 'menino' || item.gender === 'moco';
+    }
   }
 ])
+
+Array.prototype.Sum = function (attr) {
+  var numbers = this;
+  var result = 0;
+
+  for (var i = 0; i < numbers.length; i++) {
+    result += numbers[i][attr];    
+  }
+
+  return result;
+}
